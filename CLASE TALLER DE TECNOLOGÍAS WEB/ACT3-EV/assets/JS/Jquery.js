e12567288d202187d9ed39f34e8fe3ced53e42b9
Patricio Melo <42676797+patriciomelor@users.noreply.github.com>
2023-05-06 01:00:54 -0400

    Termino WEB - Faltaron cosas

--- CLASE TALLER DE TECNOLOGÍAS WEB/ACT3-EV/assets/JS/Jquery.js
+++ CLASE TALLER DE TECNOLOGÍAS WEB/ACT3-EV/assets/JS/Jquery.js
@@ -1,9 +1,42 @@
-$(function() {
-    $( "#datepicker" ).datepicker({
+var regex = /^(\+?56)?(\s?)(0?9)(\s?)[98765432]\d{7}$/;
 
-        regional: "Es",
-		showOn: "button",
-		buttonImageOnly: true,
-		buttonText: "Seleccionar fecha"
+//JQuery
+$("#telefono").keyup(function(){
+   if($("#telefono").val() == ""){
+       $("#telefono").val("02")
+   }
+   $("#telefono").val($("#telefono").val().replace(regex, ""))
+});
+
+//Javascript
+
+var numTel = document.getElementById("celular");
+
+numTel.addEventListener("keyup", function(){
+    if (numTel.value == ""){
+       numTel.value = "+56 9";
+    }
+    numTel.value = numTel.value.replace(regex,"");
+})
+
+
+function PrimeraMayus(str) {
+    const valores = document.getElementsByClassName("CNAME")
+    return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+$( function() {
+    var availableTags = [
+      "Hombre",
+      "Mujer",
+      "Indefinido",
+    ];
+    $( "#sexo" ).autocomplete({
+      source: availableTags
     });
-   });
+  } );
+
+  $(function(){
+    $nombre = document.querySelector("#nombre");
+    $nombre.focus();
+  });
